433110485374b27a1e269321a3af082ef44a35a5
soumyajit444 <soumyajitsengupta15@gmail.com>
2026-04-18 10:21:16 +0530

    Cursor Update: Implemented fluid traid cursor animation and replaced cursor icon.

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,7 +2,8 @@ import { Work_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import ThemeToggle from "@/components/ui/ThemeToggle";
-import Background from "@/components/animations/Background"; // ✅ ADD
+import Background from "@/components/animations/Background";
+import SplashCursor from "@/components/animations/SplashCursor";
 
 const workSans = Work_Sans({
   subsets: ["latin"],
@@ -17,9 +18,20 @@ export default function RootLayout({ children }) {
       <body suppressHydrationWarning>
         <ThemeToggle />
 
-        {/* ✅ GLOBAL BACKGROUND (OUTSIDE GSAP TREE) */}
         <Background />
-
+        {/* Cursor effect */}
+        <SplashCursor
+          DENSITY_DISSIPATION={3.5}
+          VELOCITY_DISSIPATION={2}
+          PRESSURE={0.1}
+          CURL={3}
+          SPLAT_RADIUS={0.2}
+          SPLAT_FORCE={6000}
+          COLOR_UPDATE_SPEED={10}
+          SHADING
+          RAINBOW_MODE={false}
+          COLOR="#4900d0"
+        />
         {children}
 
         <Toaster position="top-right" richColors />
